13a1f5798059c4f9409c48f3ed55a062b57b0c59
dbagato <dbagato@gmail.com>
2022-06-08 00:34:19 +0200

    Creacion Modelo User

---
+++ lib/models/user.dart
@@ -0,0 +1,44 @@
+import 'package:equatable/equatable.dart';
+
+class User extends Equatable {
+  final String id;
+  final String name;
+  final String lastName;
+  final int age;
+  final String gender;
+  final String? image;
+  final List<String>? positions;
+  final List<String>? arbitrageTitles;
+
+  User(this.id, this.name, this.lastName, this.age, this.gender, this.image,
+      this.positions, this.arbitrageTitles);
+
+  @override
+  List<Object?> get props => [];
+
+  Map<String, Object?> toFirebaseMap(
+      {String? newImage,
+      List<String>? newPositions,
+      List<String>? newArbitrageTitles}) {
+    return <String, Object?>{
+      'id': id,
+      'name': name,
+      'lastName': lastName,
+      'age': age,
+      'gender': gender,
+      'image': newImage ?? image,
+      'positions': newPositions ?? positions,
+      'arbitrageTitles': newArbitrageTitles ?? arbitrageTitles,
+    };
+  }
+
+  User.fromFirebaseMap(Map<String, Object?> data)
+      : id = data['id'] as String,
+        name = data['name'] as String,
+        lastName = data['lastName'] as String,
+        age = data['age'] as int,
+        gender = data['gender'] as String,
+        image = data['image'] as String?,
+        positions = data['positions'] as List<String>?,
+        arbitrageTitles = data['arbitrageTitles'] as List<String>?;
+}
