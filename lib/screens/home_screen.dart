15d22f07d0488ed45be4afdc58bda9b608e4490b
dbagato <dbagato@gmail.com>
2022-06-10 17:54:48 +0200

    navegation bug home Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:quidditch_app/services/auth_cubit.dart';
+import 'package:quidditch_app/services/userLoc_cubit.dart';
+import 'package:quidditch_app/services/userloc_repository.dart';
 import 'package:quidditch_app/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
-  static Widget create(BuildContext context) => HomeScreen();
+  static Widget create(BuildContext context) {
+    return BlocProvider(
+      create: (_) => UserLocCubit(UserLocRepository())..getUserLoc(),
+      child: HomeScreen(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: NavBarType1(),
-      body: FooterNavBar(),
-    );
+    return Scaffold(appBar: const NavBarType1(), body: FooterNavBar());
   }
 }
