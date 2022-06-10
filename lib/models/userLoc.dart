f48876befac663a12633eb03fc10e5923910e0b4
dbagato <dbagato@gmail.com>
2022-06-10 17:53:59 +0200

    filed "rol" Agree

--- lib/models/userLoc.dart
+++ lib/models/userLoc.dart
@@ -6,12 +6,13 @@ class UserLoc extends Equatable {
   final String lastName;
   final int age;
   final String gender;
+  final String rol;
   final String? image;
   final List<String>? positions;
   final List<String>? arbitrageTitles;
 
-  UserLoc(this.id, this.name, this.lastName, this.age, this.gender, this.image,
-      this.positions, this.arbitrageTitles);
+  UserLoc(this.id, this.name, this.lastName, this.age, this.gender, this.rol,
+      this.image, this.positions, this.arbitrageTitles);
 
   @override
   List<Object?> get props => [];
@@ -26,6 +27,7 @@ class UserLoc extends Equatable {
       'lastName': lastName,
       'age': age,
       'gender': gender,
+      'rol': rol,
       'image': newImage ?? image,
       'positions': newPositions ?? positions,
       'arbitrageTitles': newArbitrageTitles ?? arbitrageTitles,
@@ -38,6 +40,7 @@ class UserLoc extends Equatable {
         lastName = data['lastName'] as String,
         age = data['age'] as int,
         gender = data['gender'] as String,
+        rol = data['rol'] as String,
         image = data['image'] as String?,
         positions = data['positions'] as List<String>?,
         arbitrageTitles = data['arbitrageTitles'] as List<String>?;
