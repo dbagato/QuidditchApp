5f0e3fc4909e44e20924f32d19f02476d2cac5e5
dbagato <dbagato@gmail.com>
2022-06-10 02:21:22 +0200

    Implementacion NavBar generica

--- lib/widgets/navbar_type1.dart
+++ lib/widgets/navbar_type1.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quidditch_app/services/auth_cubit.dart';
 
-class NavBarType1 extends StatelessWidget {
+class NavBarType1 extends StatelessWidget with PreferredSizeWidget {
   const NavBarType1({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // TODO navbar generico pra los usuarios, implementar posible menu lateral
-    return AppBar();
+    return AppBar(
+      title: const Text("QuidditchApp"),
+      actions: [
+        IconButton(
+            onPressed: () => context.read<AuthCubit>().signOut(),
+            icon: const Icon(Icons.logout))
+      ],
+    );
   }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
