f3ee2f0647b9583001ecaa16b844c90d0c1da0f4
dbagato <dbagato@gmail.com>
2022-06-10 02:21:40 +0200

    Arreglo Home Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,34 +9,8 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeScreen'),
-        elevation: 5,
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            BlocBuilder<AuthCubit, AuthState>(
-              buildWhen: (previous, current) => current is AuthSignedIn,
-              builder: (_, state) {
-                final authUser = (state as AuthSignedIn).user;
-                return Center(
-                  child: Column(
-                    children: [
-                      const Text("user ID"),
-                      const SizedBox(height: 30),
-                      ElevatedButton(
-                          onPressed: () => context.read<AuthCubit>().signOut(),
-                          child: const Text("Logout"))
-                    ],
-                  ),
-                );
-              },
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: FooterNavBar(),
+      appBar: NavBarType1(),
+      body: FooterNavBar(),
     );
   }
 }
